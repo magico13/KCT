2f5c65c73e84527a81801c5d965a1e7349e76f0b
magico13 <magico1313@gmail.com>
2015-05-16 19:15:06 -0500

    Start work on presets.

---
+++ Kerbal_Construction_Time/KCT_PresetManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KerbalConstructionTime
+{
+    class KCT_PresetManager
+    {
+        public static KCT_PresetManager Instance;
+
+        public KCT_PresetManager()
+        {
+            Instance = this;
+        }
+
+    }
+
+    class KCT_Preset : ConfigNodeStorage
+    {
+        public KCT_Preset_General generalSettings;
+        public KCT_Preset_Time timeSettings;
+        public KCT_Preset_Formula formulaSettings;
+
+        [Persistent] string name, description, author;
+
+        public KCT_Preset(string presetName, string presetDescription, string presetAuthor)
+        {
+            name = presetName;
+            description = presetDescription;
+            author = presetAuthor;
+
+            generalSettings = new KCT_Preset_General();
+            timeSettings = new KCT_Preset_Time();
+            formulaSettings = new KCT_Preset_Formula();
+        }
+
+        public override void OnEncodeToConfigNode()
+        {
+            base.OnEncodeToConfigNode();
+        }
+
+        public override void OnDecodeFromConfigNode()
+        {
+            base.OnDecodeFromConfigNode();
+        }
+
+    }
+
+    class KCT_Preset_General : ConfigNodeStorage
+    {
+
+    }
+
+    class KCT_Preset_Time : ConfigNodeStorage
+    {
+
+    }
+
+    class KCT_Preset_Formula : ConfigNodeStorage
+    {
+
+    }
+}
