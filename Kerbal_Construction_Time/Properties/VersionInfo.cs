0eaf1cdfd312eeb4d6e156d51b6f0748b21f906e
magico13 <magico1313@gmail.com>
2015-11-09 15:28:55 -0600

    Fixes for 1.0.5, otherwise identical to previous version.

--- Kerbal_Construction_Time/Properties/VersionInfo.cs
+++ Kerbal_Construction_Time/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.2.1.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.2.1.0")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.2.3.0")]
+[assembly: System.Reflection.AssemblyFileVersion("1.2.3.0")]
