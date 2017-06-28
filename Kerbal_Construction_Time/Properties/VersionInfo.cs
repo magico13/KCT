782add267f2ee4ecaf8b298c3b2cb3986ce41ba9
magico13 <magico1313@gmail.com>
2017-06-27 20:17:49 -0400

    1.3.5.1 for 1.2 (apparently forgot to commit this)

--- Kerbal_Construction_Time/Properties/VersionInfo.cs
+++ Kerbal_Construction_Time/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.3.4.2")]
-[assembly: System.Reflection.AssemblyFileVersion("1.3.4.2")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.3.5.1")]
+[assembly: System.Reflection.AssemblyFileVersion("1.3.5.1")]
